67692f20aa9fd2d59c7f64f557347c7ea9e9bed4
Matheus Vieira <matheusvieira0101@gmail.com>
2024-01-27 22:50:50 -0300

    feat: Creating XlsToCSV Class and setConverter method

--- ExcelToCSV.py
+++ ExcelToCSV.py
@@ -1,3 +1,21 @@
 import pandas as pd
 import pathlib as plib
 from datetime import datetime
+
+print(f"{datetime.now().strftime("%H:%M:%S")} - Initializing Script")
+
+class XlsToCsv():
+    def __init__(self, filepath, destination_path="", decimal=",", sep=";", encoding="utf_16", compression_method="gzip") -> None:
+        self.setConverter(filepath, destination_path, decimal, sep, encoding, compression_method)
+
+    def setConverter(self, filepath, destination_path="", decimal=",", sep=";", encoding="utf_16", compression_method="gzip"):
+        print(f"{datetime.now().strftime("%H:%M:%S")} - Configuring Converter")
+
+        self.__filepath = filepath
+        self.__filename = self.__filepath[self.__filepath.rfind('\\')+1:self.__filepath.rfind(".")]+".csv"
+        self.__destination_path = destination_path
+        self.__decimal = decimal
+        self.__separator = sep
+        self.__encoding = encoding
+        self.__compression_options = dict(method=compression_method)
+        plib.Path(destination_path).mkdir(parents=True, exist_ok=True)
