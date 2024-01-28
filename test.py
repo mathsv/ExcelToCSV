c3cb962fe8c188c18be3598bdccf0bd49f877a2a
Matheus Vieira <matheusvieira0101@gmail.com>
2024-01-28 00:16:51 -0300

    test: just a few more tests

--- test.py
+++ test.py
@@ -8,15 +8,15 @@ with open("files.txt", 'r') as file:
 converter.XlsToCsv(files, r'csv')
 
 """
-23:34:32 - Initializing Script
-23:34:32 - Configuring Converter
-23:34:32 - Loading File
-23:37:00 - File dummy_file loaded!
-23:37:00 - Converting File
-23:38:04 - Files dummy_file.csv sucessfully converted!
-23:38:04 - Configuring Converter
-23:38:04 - Loading File
-23:41:09 - File dummy_file_02 successfully loaded!
-23:41:09 - Converting File
-23:42:11 - Files dummy_file_02.csv sucessfully converted!
+00:04:23 - Initializing Script
+00:04:23 - Configuring Converter
+00:04:23 - Loading File
+00:07:30 - File dummy_file successfully loaded!
+00:07:30 - Converting File
+00:08:26 - Files dummy_file.csv sucessfully converted!
+00:08:26 - Configuring Converter
+00:08:26 - Loading File
+00:11:25 - File dummy_file_02 successfully loaded!
+00:11:25 - Converting File
+00:12:28 - Files dummy_file_02.csv sucessfully converted!
 """
