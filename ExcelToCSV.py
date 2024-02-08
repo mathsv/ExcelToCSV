0b589dd0681f2e5710587e1f04f7f6800a3995a7
Matheus Vieira <matheusvieira0101@gmail.com>
2024-02-07 22:59:36 -0300

    fix: quick fix nested strings using the same quote character

--- ExcelToCSV.py
+++ ExcelToCSV.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pathlib as plib
 from datetime import datetime
 
-print(f"{datetime.now().strftime("%H:%M:%S")} - Initializing Script")
+print(f"{datetime.now().strftime('%H:%M:%S')} - Initializing Script")
 class XlsToCsv():
     def __init__(self, filepath:list|str, destination_path="", decimal=",", sep=";", encoding="utf_16", compression_method="gzip") -> None:
         if isinstance(filepath, str):
@@ -15,7 +15,7 @@ class XlsToCsv():
         
 
     def setConverter(self, filepath, destination_path="", decimal=",", sep=";", encoding="utf_16", compression_method="gzip"):
-        print(f"{datetime.now().strftime("%H:%M:%S")} - Configuring Converter")
+        print(f"{datetime.now().strftime('%H:%M:%S')} - Configuring Converter")
 
         self.__filepath = filepath
         self.__filename = self.__filepath[self.__filepath.rfind('\\')+1:self.__filepath.rfind(".")]+".csv"
@@ -27,12 +27,12 @@ class XlsToCsv():
         plib.Path(destination_path).mkdir(parents=True, exist_ok=True)
 
     def converter(self):
-        print(f"{datetime.now().strftime("%H:%M:%S")} - Loading File")
+        print(f"{datetime.now().strftime('%H:%M:%S')} - Loading File")
         read_file = pd.read_excel(plib.Path(self.__filepath))
-        print(f"{datetime.now().strftime("%H:%M:%S")} - File {self.__filename[:-4]} successfully loaded!")
+        print(f"{datetime.now().strftime('%H:%M:%S')} - File {self.__filename[:-4]} successfully loaded!")
 
-        print(f"{datetime.now().strftime("%H:%M:%S")} - Converting File")
+        print(f"{datetime.now().strftime('%H:%M:%S')} - Converting File")
         newPath = plib.Path(f'{self.__destination_path}/{self.__filename}.gz')
         read_file.to_csv(newPath, index = None, header=True, decimal=self.__decimal, sep=self.__separator,
                          encoding=self.__encoding,compression=self.__compression_options)
-        print(f"{datetime.now().strftime("%H:%M:%S")} - Files {self.__filename} sucessfully converted!")
+        print(f"{datetime.now().strftime('%H:%M:%S')} - Files {self.__filename} sucessfully converted!")
